affa1c6a33168de8c71b6197025df9dd383001f2
karthik4754 <karthikcreative14@gmail.com>
2020-05-02 00:14:20 +0100

    mave3

--- Task2.java
+++
@@ -1,5 +0,0 @@
-package org.sample;
-
-public class Task2 {
-
-}
---
+++ mav3.java
@@ -0,0 +1,8 @@
+package org.sample;
+
+public class mav3 {
+	public static void main(String[] args) {
+		System.out.println("greens");
+	}
+
+}
